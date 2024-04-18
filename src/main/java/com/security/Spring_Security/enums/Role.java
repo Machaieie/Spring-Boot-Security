009d2b5496f7ffc7995ab637250ecbd38cca42f6
Machaieie <machaieieedwin1@gmail.com>
2024-04-18 16:41:23 +0200

    upload do projecto

---
+++ src/main/java/com/security/Spring_Security/enums/Role.java
@@ -0,0 +1,6 @@
+package com.security.Spring_Security.enums;
+
+public enum Role {
+    ADMIN,
+    EMPLOYEE
+}
---
+++ src/main/java/com/security/Spring_Security/model/Usuario.java
@@ -0,0 +1,5 @@
+package com.security.Spring_Security.model;
+
+public class Usuario {
+    
+}
